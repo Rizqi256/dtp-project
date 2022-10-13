bb26072fc5d7bd9a25ee414dfb7f88e57f5b4f2e
Muhammad Nur Rizqi <rzq0506@gmail.com>
2022-10-13 12:48:12 +0700

    first

---
+++ resources/views/student.blade.php
@@ -0,0 +1,18 @@
+@extends('templates.default')
+
+@php
+    $title = 'HMMM';
+    $pretitle = 'hhhh'
+@endphp
+
+@section('content')
+<h1>hai</h1>
+@endsection
+
+@push('page-action')
+    <div class="btn btn-primary">add add</div>
+@endpush
+
+@push('extra-script')
+    
+@endpush
---
+++ resources/views/test.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.default')
+
+@section('content')
+<h1>hmm</h1>
+@endsection
